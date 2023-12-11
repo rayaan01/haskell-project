9ac28ac03bf193ad77a4c4806e70a98bf92fc3f6
kotuw <46974369+KotUW@users.noreply.github.com>
2023-12-11 15:27:58 +0000

    download zip file

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
 import Lib
+import Network.HTTP.Simple
+import qualified Data.ByteString.Char8 as B8
 
 main :: IO ()
-main = someFunc
+main = do
+  let gdp_url = "https://api.worldbank.org/v2/en/indicator/NY.GDP.MKTP.CD?downloadformat=xml"
+  responce <- httpBS gdp_url
+  -- putStrLn $ show $ getResponseStatusCode responce
+  B8.writeFile "gdp.zip" $ getResponseBody responce
+  
