41b6739a8f96bf27744a1f8c515437dc662a66b4
kotuw <46974369+KotUW@users.noreply.github.com>
2023-12-11 16:28:26 +0000

    [feat] Added data sources

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,18 @@
 module Main (main) where
 
-import Fetch (downloadZip)
+import Fetch (downloadFile)
+
+data URLS = URLS {
+  gdp :: String,
+  pop :: String,
+  isZip :: Bool
+} deriving (Show)
 
 main :: IO ()
 main = do
-  let gdp_url = "https://api.worldbank.org/v2/en/indicator/NY.GDP.MKTP.CD?downloadformat=xml"
-  downloadZip gdp_url "gdp.zip"
-  -- responce <- httpBS gdp_url
-  -- putStrLn $ show $ getResponseStatusCode responce
-  -- B8.writeFile "gdp.zip" $ getResponseBody responce
+
+  let dataUN = URLS {gdp = "https://data.un.org/_Docs/SYB/CSV/SYB66_230_202310_GDP%20and%20GDP%20Per%20Capita.csv", pop = "https://data.un.org/_Docs/SYB/CSV/SYB66_1_202310_Population,%20Surface%20Area%20and%20Density.csv", isZip = True}
+  
+  let dataWorldBank = URLS {gdp = "https://data.un.org/_Docs/SYB/CSV/SYB66_1_202310_Population,%20Surface%20Area%20and%20Density.csv", pop = "https://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=xml", isZip = False}
+  downloadFile (gdp dataWorldBank) "gdp.zip"
   
--- src/Fetch.hs
+++ src/Fetch.hs
@@ -1,10 +1,10 @@
-module Fetch (downloadZip) where
+module Fetch (downloadFile) where
 
 import Network.HTTP.Simple
 import qualified Data.ByteString.Char8 as B8
 
-downloadZip :: String -> String -> IO ()
-downloadZip url filename = do
+downloadFile :: String -> String -> IO ()
+downloadFile url filename = do
   let req = parseRequest_ url
   responce <- httpBS req
   -- putStrLn $ show $ getResponseStatusCode responce
