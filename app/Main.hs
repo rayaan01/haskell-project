334f6037d2efbedb2a64afe1cdec616cd455ca60
kotuw <46974369+KotUW@users.noreply.github.com>
2023-12-11 15:37:05 +0000

    Working function

--- app/Main.hs
+++ app/Main.hs
@@ -5,10 +5,19 @@ import Lib
 import Network.HTTP.Simple
 import qualified Data.ByteString.Char8 as B8
 
+downloadZip :: String -> String -> IO ()
+downloadZip url filename = do
+  let req = parseRequest_ url
+  responce <- httpBS req
+  -- putStrLn $ show $ getResponseStatusCode responce
+  B8.writeFile filename  $ getResponseBody responce
+  
+
 main :: IO ()
 main = do
   let gdp_url = "https://api.worldbank.org/v2/en/indicator/NY.GDP.MKTP.CD?downloadformat=xml"
-  responce <- httpBS gdp_url
+  downloadZip gdp_url "gdp.zip"
+  -- responce <- httpBS gdp_url
   -- putStrLn $ show $ getResponseStatusCode responce
-  B8.writeFile "gdp.zip" $ getResponseBody responce
+  -- B8.writeFile "gdp.zip" $ getResponseBody responce
   
