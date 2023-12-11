80e909d6b61d57425dc9bdf7e17cc0f228d8b7ff
kotuw <46974369+KotUW@users.noreply.github.com>
2023-12-11 17:08:20 +0000

    Completed implementation of Fetch

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,13 @@
 module Main (main) where
 
-import Fetch (downloadFile)
-
-data URLS = URLS {
-  gdp :: String,
-  pop :: String,
-  isZip :: Bool
-} deriving (Show)
+import Fetch (downloadURLS, URLS(..))
 
 main :: IO ()
 main = do
 
-  let dataUN = URLS {gdp = "https://data.un.org/_Docs/SYB/CSV/SYB66_230_202310_GDP%20and%20GDP%20Per%20Capita.csv", pop = "https://data.un.org/_Docs/SYB/CSV/SYB66_1_202310_Population,%20Surface%20Area%20and%20Density.csv", isZip = True}
+  let dataUN = URLS {gdp = "https://data.un.org/_Docs/SYB/CSV/SYB66_230_202310_GDP%20and%20GDP%20Per%20Capita.csv", pop = "https://data.un.org/_Docs/SYB/CSV/SYB66_1_202310_Population,%20Surface%20Area%20and%20Density.csv", isZip = False}
   
-  let dataWorldBank = URLS {gdp = "https://data.un.org/_Docs/SYB/CSV/SYB66_1_202310_Population,%20Surface%20Area%20and%20Density.csv", pop = "https://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=xml", isZip = False}
-  downloadFile (gdp dataWorldBank) "gdp.zip"
+  let dataWorldBank = URLS {gdp = "https://data.un.org/_Docs/SYB/CSV/SYB66_1_202310_Population,%20Surface%20Area%20and%20Density.csv", pop = "https://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=xml", isZip = True}
+
+  downloadURLS dataWorldBank 
   
--- src/Fetch.hs
+++ src/Fetch.hs
@@ -1,8 +1,20 @@
-module Fetch (downloadFile) where
+module Fetch (downloadURLS, URLS(..)) where
 
 import Network.HTTP.Simple
 import qualified Data.ByteString.Char8 as B8
 
+data URLS = URLS {
+  gdp :: String,
+  pop :: String,
+  isZip :: Bool
+} deriving (Show)
+
+downloadURLS :: URLS -> IO ()
+downloadURLS datasite = do
+  let ext = if (isZip datasite ) then ".zip" else ".csv"
+  downloadFile (gdp datasite) ("gdp" ++ ext)
+  downloadFile (pop datasite) ("pop" ++ ext)
+    
 downloadFile :: String -> String -> IO ()
 downloadFile url filename = do
   let req = parseRequest_ url
