42164eb1691a5909e7f136a41eaa24791f733541
kotuw <46974369+KotUW@users.noreply.github.com>
2023-12-12 14:49:34 +0000

    Using Missing H

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,11 @@
 module Main (main) where
-import Lib
-import Control.Applicative
-import qualified Data.ByteString.Lazy as BL
-import Data.Typeable
-import Data.Csv
-import qualified Data.Vector as V
+
+import Data.CSV
+import Text.ParserCombinators.Parsec
 
 removeComma = filter (/= ',')
 
 main :: IO ()
 main = do
-    csvData <- BL.readFile "new_gdp.csv"
-    case decode NoHeader csvData of
-        Left err -> putStrLn err
-        Right v -> V.forM_ v $ \ (country, year, gdp) -> do
-            putStrLn("It's a country")
+    result <- parseFromFile csvFile "gdp.csv"
+    putStrLn $ show result
