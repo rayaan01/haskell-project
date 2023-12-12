de1ae6926bd1e9dfc5d87e47c121d59b76cbe02a
Rayaan Hussain <md.rayaan.h@gmail.com>
2023-12-12 14:01:02 +0000

    fix: remove shit

--- app/Main.hs
+++ app/Main.hs
@@ -1,50 +1,17 @@
 module Main (main) where
-
 import Lib
+import Control.Applicative
 import qualified Data.ByteString.Lazy as BL
 import Data.Typeable
-import Control.Monad.State
 import Data.Csv
 import qualified Data.Vector as V
 
 removeComma = filter (/= ',')
 
-data MyState = MyState { startParsing :: Bool }
-
-updateState :: Bool -> State MyState ()
-updateState newValue = modify (\s -> s { startParsing = newValue })
-
-useState :: State MyState String
-useState = do
-    value <- gets startParsing
-    return $ "Start Parsing value: " ++ show value
-
 main :: IO ()
 main = do
     csvData <- BL.readFile "new_gdp.csv"
-    let initialState = MyState { startParsing = False }
     case decode NoHeader csvData of
         Left err -> putStrLn err
         Right v -> V.forM_ v $ \ (country, year, gdp) -> do
-            let (result, finalState) = runState(do
-                if country == "Afghanistan"
-                    then updateState True
-                    else updateState False
-
-                    result <- useState
-                    return result
-                    ) initialState
-
-
-            -- if (startParsing)
-            --     then do
-            --         putStrLn("New Country")
-            --     else
-            --         putStrLn("Not a country")
-
-            let noCommaGDP = removeComma gdp
-            let intGDP = read noCommaGDP :: Int
-            putStrLn ("The country is " ++ country ++ "\nThe year is " ++ year ++ "\nThe GDP " ++ noCommaGDP ++ "\nInt " ++ show intGDP)
-            putStrLn ("Type of country: " ++ show (typeOf country))
-            putStrLn ("Type of year: " ++ show (typeOf year))
-            putStrLn ("Type of gdp: " ++ show (typeOf intGDP))
+            putStrLn("It's a country")
