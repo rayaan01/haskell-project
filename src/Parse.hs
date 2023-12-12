45491c118e4a7966eff49a2102efdd4d1459ecfe
kotuw <46974369+KotUW@users.noreply.github.com>
2023-12-12 22:12:50 +0000

    Working parsing of a GDP record

--- src/Parse.hs
+++ src/Parse.hs
@@ -1,9 +1,10 @@
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 module Parse (parseCSV) where
 
 import Data.CSV
 import Text.ParserCombinators.Parsec
 import Data.Either
-import Data.Typeable
+-- import Data.Typeable
 
 data RecordGDP = GDP {
   id :: Int,
@@ -12,24 +13,26 @@ data RecordGDP = GDP {
   y_2010 :: Int,
   y_2015 :: Int,
   y_2020 :: Int,
-  capital :: String 
+  capital :: String
 }
 
 data RecordCsv = Record {
-  r_id :: String,
+  r_id :: Int,
   r_country :: String,
-  year:: String, 
-  gdp :: String
+  year:: String,
+  gdp :: Int
 } deriving (Show)
 
 parseRecord :: [String] -> RecordCsv
-parseRecord record = Record { r_id = (record !! 0) , r_country = (record !! 1), year = (record !! 2), gdp = (record !! 4) } 
+parseRecord record = Record { r_id = read (head record) , r_country = record !! 1, year = record !! 2, gdp = read $ filter (/=',') (record !! 4 )}
 
 
 parseGDP :: [[String]] ->  IO ()
 parseGDP csvData  = do
   -- print $ typeOf $ (take 1 csvData) !! 0
-  print $ parseRecord $ (take 1 csvData) !! 0
+  -- print $ parseRecord $ (take 1 csvData) !! 0
+  let csvRecord =  map parseRecord csvData
+  print $ head csvRecord
 
 parseCSV :: String -> IO ()
 parseCSV fileCSV = do
