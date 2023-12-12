f1b92692d39f7b1e483c360351cece22fcbacb17
Koustubh Saxena <46974369+KotUW@users.noreply.github.com>
2023-12-12 14:18:30 +0000

    Update Main.hs
    Updated worldbank API's to correct URLs

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ main = do
 
   let dataUN = URLS {gdp = "https://data.un.org/_Docs/SYB/CSV/SYB66_230_202310_GDP%20and%20GDP%20Per%20Capita.csv", pop = "https://data.un.org/_Docs/SYB/CSV/SYB66_1_202310_Population,%20Surface%20Area%20and%20Density.csv", isZip = False}
   
-  let dataWorldBank = URLS {gdp = "https://data.un.org/_Docs/SYB/CSV/SYB66_1_202310_Population,%20Surface%20Area%20and%20Density.csv", pop = "https://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=xml", isZip = True}
+  let dataWorldBank = URLS {gdp = "https://api.worldbank.org/v2/en/indicator/NY.GDP.MKTP.CD?downloadformat=csv", pop = "https://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=csv", isZip = True}
 
   downloadURLS dataWorldBank 
   
