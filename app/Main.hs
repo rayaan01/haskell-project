c9d41251ede27f4df4c41850e36d153affc9d880
kotuw <46974369+KotUW@users.noreply.github.com>
2023-12-13 20:44:20 +0000

    [WIP] managing files for simple interface

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,8 @@
 module Main (main) where
 
-import Fetch (downloadURLS, URLS(..))
+import Fetch 
 import Parse
+import Types
 
 -- | The Main function
 main :: IO ()
@@ -10,6 +11,10 @@ main = do
   let dataUN = URLS {gdp = "https://data.un.org/_Docs/SYB/CSV/SYB66_230_202310_GDP%20and%20GDP%20Per%20Capita.csv", pop = "https://data.un.org/_Docs/SYB/CSV/SYB66_1_202310_Population,%20Surface%20Area%20and%20Density.csv", isZip = False}
   
   let dataWorldBank = URLS {gdp = "https://api.worldbank.org/v2/en/indicator/NY.GDP.MKTP.CD?downloadformat=csv", pop = "https://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=csv", isZip = True}
+
+  -- _ = if not $ checkURLS dataUN then return () else True
+
+  -- let files = CSVFiles { gdpf "gdp", popf "pop"}
 
   downloadURLS dataUN
 
