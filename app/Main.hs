07f0e806c40c122f75a19f4027d042dfe706d9a4
soham7372 <soham.khawte2024@gmail.com>
2023-12-13 19:27:08 +0000

    Added functions to recursively insert all data into tables.

--- app/Main.hs
+++ app/Main.hs
@@ -7,25 +7,23 @@ import Database.SQLite.Simple
 import Database.SQLite.Simple.FromRow           
 import Data.Time                                
 
-main :: IO ()
-main = print "db-lesson"
+-- main :: IO ()
+-- main = print "db-lesson"
 
 data Population = Population
  { countryID :: Int
  , countryNameP :: String
- , pop2005 :: Int
  , pop2010 :: Int
  , pop2015 :: Int
- , pop2020 :: Int
+ , pop2021 :: Int
  , capital :: String
  }
 
 data GDP = GDP
  { countryNameG :: String
- , gdp2005 :: Int
  , gdp2010 :: Int
  , gdp2015 :: Int
- , gdp2020 :: Int
+ , gdp2021 :: Int
  }
 
 instance Show Population where
@@ -34,41 +32,72 @@ instance Show Population where
                              , countryNameP population
                              , ", Capital: "
                              , capital population
-                             , ", Population in 2005: "
-                             , show $ pop2005 population
                              , ", 2010: "
                              , show $ pop2010 population
                              , ", 2015: "
                              , show $ pop2015 population
-                             , ", 2020: "
-                             , show $ pop2020 population
+                             , ", 2021: "
+                             , show $ pop2021 population
                              , "\n"]
 
 instance Show GDP where
    show gdp = mconcat [ countryNameG gdp
-                      , ", GDP in 2005: "
-                      , show $ gdp2005 gdp
                       , ", 2010: "
                       , show $ gdp2010 gdp
                       , ", 2015: "
                       , show $ gdp2015 gdp
-                      , ", 2020: "
-                      , show $ gdp2020 gdp
+                      , ", 2021: "
+                      , show $ gdp2021 gdp
                       , "\n"]
 
 
-addPopulation :: Int -> String -> String -> Int -> Int -> Int -> Int -> IO ()
-addPopulation countryID countryNameP capital pop2005 pop2010 pop2015 pop2020 = withConn "tools.db" $ \conn -> do
-   execute conn "INSERT INTO POPULATION (countryID, countryName, capital, 2005, 2010, 2015, 2020) VALUES (?,?,?,?,?,?,?)" (countryID, countryNameP, capital, pop2005, pop2010, pop2015, pop2020)                                            
-   putStrLn "Population data added"
+-- addPopulation :: Int -> String -> String -> Int -> Int -> Int -> IO ()
+-- addPopulation countryID countryNameP capital pop2010 pop2015 pop2021 = withConn "tools.db" $ \conn -> do
+--    execute conn "INSERT INTO POPULATION (countryID, countryName, capital, 2010, 2015, 2021) VALUES (?,?,?,?,?,?)" (countryID, countryNameP, capital, pop2010, pop2015, pop2021)                                            
+--    putStrLn "Population data added"
 
-addGDP :: String -> Int -> Int -> Int -> Int -> IO ()
-addGDP countryNameG gdp2005 gdp2010 gdp2015 gdp2020 = withConn "tools.db" $ \conn -> do
-   execute conn "INSERT INTO GDP (countryName, 2005, 2010, 2015, 2020) VALUES (?,?,?,?,?)" (countryNameG, gdp2005, gdp2010, gdp2015, gdp2020)                                            
-   putStrLn "GDP data added"
+-- addGDP :: String -> Int -> Int -> Int -> IO ()
+-- addGDP countryNameG gdp2010 gdp2015 gdp2021 = withConn "tools.db" $ \conn -> do
+--    execute conn "INSERT INTO GDP (countryName, 2010, 2015, 2021) VALUES (?,?,?,?)" (countryNameG, gdp2010, gdp2015, gdp2021)                                            
+--    putStrLn "GDP data added"
 
 withConn :: String -> (Connection -> IO ()) -> IO ()
 withConn dbName action = do
    conn <- open dbName
    action conn
    close conn
+
+
+addPopulation :: (Int, String, String, Int, Int, Int) -> IO ()
+addPopulation (countryID, countryNameP, capital, pop2010, pop2015, pop2021) = withConn "tools.db" $ \conn -> do
+   execute conn "INSERT INTO POPULATION (countryID, countryNameP, capital, pop2010, pop2015, pop2021) VALUES (?,?,?,?,?,?)" (countryID, countryNameP, capital, pop2010, pop2015, pop2021)                                            
+  --  putStrLn "Population data added"
+
+addGDP :: (String, Int, Int, Int) -> IO ()
+addGDP (countryNameG, gdp2010, gdp2015, gdp2021) = withConn "tools.db" $ \conn -> do
+   execute conn "INSERT INTO GDP (countryNameG, gdp2010, gdp2015, gdp2021) VALUES (?,?,?,?)" (countryNameG, gdp2010, gdp2015, gdp2021)                                            
+  --  putStrLn "GDP data of added"
+
+-- Assuming popData and gdpData are lists of tuples...
+popData :: [(Int, String, String, Int, Int, Int)]
+popData = [(3, "USA", "Delhi", 100, 200, 300), (4, "Russia", "Karachi", 100, 200, 300)]
+
+gdpData :: [(String, Int, Int, Int)]
+gdpData = [("USA", 10, 30, 25), ("Russia", 11, 31, 30)]
+
+-- Use mapM_ to apply addPopulation and addGDP to each element in popData and gdpData
+createTables :: IO ()
+createTables = withConn "tools.db" $ \conn -> do
+    execute_ conn "DROP TABLE IF EXISTS POPULATION;"
+    execute_ conn "DROP TABLE IF EXISTS GDP;"
+    execute_ conn "CREATE TABLE POPULATION (countryID INTEGER PRIMARY KEY, countryNameP TEXT, capital TEXT, pop2010 INTEGER, pop2015 INTEGER, pop2021 INTEGER);"
+    execute_ conn "CREATE TABLE GDP (countryNameG TEXT PRIMARY KEY, gdp2010 FLOAT, gdp2015 FLOAT, gdp2021 FLOAT);"
+    putStrLn "Tables created"
+
+-- Use mapM_ to apply addPopulation and addGDP to each element in popData and gdpData
+main :: IO ()
+main = do
+    createTables
+    mapM_ addPopulation popData
+    mapM_ addGDP gdpData
+    putStrLn "Data Added Succesfully!"
--- app/database.hs
+++ app/database.hs
@@ -1,48 +1,65 @@
-module Database where
+
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
 
 import Control.Applicative
 import Database.SQLite.Simple                   
 import Database.SQLite.Simple.FromRow           
-import Data.Time  
+import Data.Time                                
 
-data Tool = Tool
- { toolId :: Int
- , name :: String
- , description :: String
- , lastReturned :: Day
- , timesBorrowed :: Int
+-- main :: IO ()
+-- main = print "db-lesson"
+
+data Population = Population
+ { countryID :: Int
+ , countryNameP :: String
+ , pop2010 :: Int
+ , pop2015 :: Int
+ , pop2021 :: Int
+ , capital :: String
  }
 
- data User = User
- { userId :: Int
- , userName :: String
+data GDP = GDP
+ { countryNameG :: String
+ , gdp2010 :: Int
+ , gdp2015 :: Int
+ , gdp2021 :: Int
  }
 
- instance Show User where
-   show user = mconcat [ show $ userId user
-                       , ".)  "
-                       , userName user]
+instance Show Population where
+   show population = mconcat [ show $ countryID population
+                             , ".) "
+                             , countryNameP population
+                             , ", Capital: "
+                             , capital population
+                             , ", 2010: "
+                             , show $ pop2010 population
+                             , ", 2015: "
+                             , show $ pop2015 population
+                             , ", 2021: "
+                             , show $ pop2021 population
+                             , "\n"]
 
-instance Show Tool where
-   show tool = mconcat [ show $ toolId tool
-                       , ".) "
-                       , name tool
-                       , "\n description: "
-                       , description tool
-                       , "\n last returned: "
-                       , show $ lastReturned tool
-                       , "\n times borrowed: "
-                       , show $ timesBorrowed tool
-                       , "\n"]
+instance Show GDP where
+   show gdp = mconcat [ countryNameG gdp
+                      , ", 2010: "
+                      , show $ gdp2010 gdp
+                      , ", 2015: "
+                      , show $ gdp2015 gdp
+                      , ", 2021: "
+                      , show $ gdp2021 gdp
+                      , "\n"]
 
-addUser :: String -> IO ()
-addUser userName = do
-   conn <- open "tools.db"                                      
-   execute conn "INSERT INTO users (username) VALUES (?)"       
-     (Only userName)                                            
-     print "user added"
-   close conn  
 
+-- addPopulation :: Int -> String -> String -> Int -> Int -> Int -> IO ()
+-- addPopulation countryID countryNameP capital pop2010 pop2015 pop2021 = withConn "tools.db" $ \conn -> do
+--    execute conn "INSERT INTO POPULATION (countryID, countryName, capital, 2010, 2015, 2021) VALUES (?,?,?,?,?,?)" (countryID, countryNameP, capital, pop2010, pop2015, pop2021)                                            
+--    putStrLn "Population data added"
+
+-- addGDP :: String -> Int -> Int -> Int -> IO ()
+-- addGDP countryNameG gdp2010 gdp2015 gdp2021 = withConn "tools.db" $ \conn -> do
+--    execute conn "INSERT INTO GDP (countryName, 2010, 2015, 2021) VALUES (?,?,?,?)" (countryNameG, gdp2010, gdp2015, gdp2021)                                            
+--    putStrLn "GDP data added"
 
 withConn :: String -> (Connection -> IO ()) -> IO ()
 withConn dbName action = do
@@ -51,10 +68,36 @@ withConn dbName action = do
    close conn
 
 
-checkout :: Int -> Int -> IO ()
-checkout userId toolId = withConn "tools.db" $
-                         \conn -> do
-                           execute conn
-                             "INSERT INTO checkedout
-                             (user_id,tool_id) VALUES (?,?)"
-                             (userId,toolId)
+addPopulation :: (Int, String, String, Int, Int, Int) -> IO ()
+addPopulation (countryID, countryNameP, capital, pop2010, pop2015, pop2021) = withConn "tools.db" $ \conn -> do
+   execute conn "INSERT INTO POPULATION (countryID, countryNameP, capital, pop2010, pop2015, pop2021) VALUES (?,?,?,?,?,?)" (countryID, countryNameP, capital, pop2010, pop2015, pop2021)                                            
+  --  putStrLn "Population data added"
+
+addGDP :: (String, Int, Int, Int) -> IO ()
+addGDP (countryNameG, gdp2010, gdp2015, gdp2021) = withConn "tools.db" $ \conn -> do
+   execute conn "INSERT INTO GDP (countryNameG, gdp2010, gdp2015, gdp2021) VALUES (?,?,?,?)" (countryNameG, gdp2010, gdp2015, gdp2021)                                            
+  --  putStrLn "GDP data of added"
+
+-- Assuming popData and gdpData are lists of tuples...
+popData :: [(Int, String, String, Int, Int, Int)]
+popData = [(3, "USA", "Delhi", 100, 200, 300), (4, "Russia", "Karachi", 100, 200, 300)]
+
+gdpData :: [(String, Int, Int, Int)]
+gdpData = [("USA", 10, 30, 25), ("Russia", 11, 31, 30)]
+
+-- Use mapM_ to apply addPopulation and addGDP to each element in popData and gdpData
+createTables :: IO ()
+createTables = withConn "tools.db" $ \conn -> do
+    execute_ conn "DROP TABLE IF EXISTS POPULATION;"
+    execute_ conn "DROP TABLE IF EXISTS GDP;"
+    execute_ conn "CREATE TABLE POPULATION (countryID INTEGER PRIMARY KEY, countryNameP TEXT, capital TEXT, pop2010 INTEGER, pop2015 INTEGER, pop2021 INTEGER);"
+    execute_ conn "CREATE TABLE GDP (countryNameG TEXT PRIMARY KEY, gdp2010 FLOAT, gdp2015 FLOAT, gdp2021 FLOAT);"
+    putStrLn "Tables created"
+
+-- Use mapM_ to apply addPopulation and addGDP to each element in popData and gdpData
+main :: IO ()
+main = do
+    createTables
+    mapM_ addPopulation popData
+    mapM_ addGDP gdpData
+    putStrLn "Data Added Succesfully!"
