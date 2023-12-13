f2a76c16ca1ee223045abc71aa6e1850232c54ae
kotuw <46974369+KotUW@users.noreply.github.com>
2023-12-13 21:44:53 +0000

    Fixed MAin

--- app/Main.hs
+++ app/Main.hs
@@ -8,9 +8,9 @@ import Database
 main :: IO ()
 main = do
   -- Fetching
-  let dataUN = URLS {gdp = "https://data.un.org/_Docs/SYB/CSV/SYB66_230_202310_GDP%20and%20GDP%20Per%20Capita.csv", pop = "https://data.un.org/_Docs/SYB/CSV/SYB66_1_202310_Population,%20Surface%20Area%20and%20Density.csv", isZip = False}
+  let dataUN = URLS {gdp_url = "https://data.un.org/_Docs/SYB/CSV/SYB66_230_202310_GDP%20and%20GDP%20Per%20Capita.csv", pop_url = "https://data.un.org/_Docs/SYB/CSV/SYB66_1_202310_Population,%20Surface%20Area%20and%20Density.csv", isZip = False}
   
-  let dataWorldBank = URLS {gdp = "https://api.worldbank.org/v2/en/indicator/NY.GDP.MKTP.CD?downloadformat=csv", pop = "https://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=csv", isZip = True}
+  let dataWorldBank = URLS {gdp_url = "https://api.worldbank.org/v2/en/indicator/NY.GDP.MKTP.CD?downloadformat=csv", pop_url = "https://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=csv", isZip = True}
 
   downloadURLS dataUN
 
@@ -21,6 +21,6 @@ main = do
   -- Database
 
   createTables
-    mapM_ addPopulation popData
-    mapM_ addGDP gdpData
-    putStrLn "Data Added Succesfully!"
+  mapM_ addPopulation popData
+  mapM_ addGDP gdpData
+  putStrLn "Data Added Succesfully!"
--- src/Fetch.hs
+++ src/Fetch.hs
@@ -5,8 +5,8 @@ import qualified Data.ByteString.Char8 as B8
 
 data URLS = -- | URLS is used to save urls from which to download the data. 
   URLS {
-  gdp :: String, -- ^ url from which to download GDP 
-  pop :: String, -- ^ url from which to download pop
+  gdp_url :: String, -- ^ url from which to download GDP 
+  pop_url :: String, -- ^ url from which to download pop
   isZip :: Bool  -- ^ bool to know if the link gives  a zip or not
 } deriving (Show)
 
@@ -14,8 +14,8 @@ data URLS = -- | URLS is used to save urls from which to download the data.
 downloadURLS :: URLS -> IO ()
 downloadURLS datasite = do
   let ext = if (isZip datasite ) then ".zip" else ".csv"
-  downloadFile (gdp datasite) ("gdp" ++ ext)
-  downloadFile (pop datasite) ("pop" ++ ext)
+  downloadFile (gdp_url datasite) ("gdp" ++ ext)
+  downloadFile (pop_url datasite) ("pop" ++ ext)
 
 -- | downloads a file of an internet and saves the output to the given file.    
 downloadFile :: String -> String -> IO ()
