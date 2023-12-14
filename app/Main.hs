ec3e7387f35e1f82cf36a8233c38c44118d0109e
Rayaan Hussain <md.rayaan.h@gmail.com>
2023-12-14 13:41:56 +0000

    fix: modudle

--- app/Main.hs
+++ app/Main.hs
@@ -8,17 +8,14 @@ import Database
 -- | The Main function
 main :: IO ()
 main = do
-  -- Fetching
-  let dataUn = getURLConstructor "UN"
-
-  downloadURLS dataUn
+  -- Fetching - Can pass "UN" or "World Bank"
+  let dataUN = getURLConstructor "UN"
+  downloadURLS dataUN
 
   -- Parsing
-
   parseCSV "gdp.csv"
 
   -- Database Operations
-
   createTables
   mapM_ addPopulation popData
   mapM_ addGDP gdpData
