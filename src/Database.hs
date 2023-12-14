86a8ce37319981c262f33aa1ba6e7264780a1cd0
vivekrambh <vivekrambha@gmail.com>
2023-12-14 17:06:29 +0000

    Fixed types conflict

--- src/Database.hs
+++ src/Database.hs
@@ -1,53 +1,11 @@
-
 {-# LANGUAGE OverloadedStrings #-}
 module Database where
 
-import Control.Applicative
 import Database.SQLite.Simple                   
 import Database.SQLite.Simple.FromRow           
 import Data.Time    
 import Types
 import Data.Char (isDigit)                            
-
-data Population = Population
- { countryID :: Int
- , countryNameP :: String
- , pop2010 :: String
- , pop2015 :: String
- , pop2021 :: String
- , capital :: String
- }
-
-data GDP = GDP
- { countryNameG :: String
- , gdp2010 :: Int
- , gdp2015 :: Int
- , gdp2021 :: Int
- }
-
-instance Show Population where
-   show population = mconcat [ show $ countryID population
-                             , ".) "
-                             , countryNameP population
-                             , ", Capital: "
-                             , capital population
-                             , ", 2010: "
-                             , show $ pop2010 population
-                             , ", 2015: "
-                             , show $ pop2015 population
-                             , ", 2021: "
-                             , show $ pop2021 population
-                             , "\n"]
-
-instance Show GDP where
-   show gdp = mconcat [ countryNameG gdp
-                      , ", 2010: "
-                      , show $ gdp2010 gdp
-                      , ", 2015: "
-                      , show $ gdp2015 gdp
-                      , ", 2021: "
-                      , show $ gdp2021 gdp
-                      , "\n"]
 
 withConn :: String -> (Connection -> IO ()) -> IO ()
 withConn dbName action = do
