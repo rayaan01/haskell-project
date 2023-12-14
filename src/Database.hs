8b091ad0fc7de9954f99c0f9776a351bfd1f828f
kotuw <46974369+KotUW@users.noreply.github.com>
2023-12-14 16:49:51 +0000

    Moved Database types to Types.hs

--- src/Database.hs
+++ src/Database.hs
@@ -9,46 +9,6 @@ import Data.Time
 import Types
 import Data.Char (isDigit)                            
 
-data Population = Population
- { countryID :: Int
- , countryNameP :: String
- , pop2010 :: Int
- , pop2015 :: Int
- , pop2021 :: Int
- , capital :: String
- }
-
-data GDP = GDP
- { countryNameG :: String
- , gdp2010 :: Int
- , gdp2015 :: Int
- , gdp2021 :: Int
- }
-
-instance Show Population where
-   show population = mconcat [ show $ countryID population
-                             , ".) "
-                             , countryNameP population
-                             , ", Capital: "
-                             , capital population
-                             , ", 2010: "
-                             , show $ pop2010 population
-                             , ", 2015: "
-                             , show $ pop2015 population
-                             , ", 2021: "
-                             , show $ pop2021 population
-                             , "\n"]
-
-instance Show GDP where
-   show gdp = mconcat [ countryNameG gdp
-                      , ", 2010: "
-                      , show $ gdp2010 gdp
-                      , ", 2015: "
-                      , show $ gdp2015 gdp
-                      , ", 2021: "
-                      , show $ gdp2021 gdp
-                      , "\n"]
-
 withConn :: String -> (Connection -> IO ()) -> IO ()
 withConn dbName action = do
    conn <- open dbName
--- src/Types.hs
+++ src/Types.hs
@@ -1,4 +1,4 @@
-module Types (URLS(..), CSVFiles(..), RecordGDP(..), RecordPOP(..)) where
+module Types (URLS(..), CSVFiles(..), RecordGDP(..), RecordPOP(..), Population(..), GDP(..)) where
 
 data URLS = -- | URLS is used to save urls from which to download the data. 
   URLS {
@@ -26,3 +26,43 @@ data RecordPOP = RecordPOP {
   p_year :: String,
   pop :: String
 } deriving (Show)
+
+data Population = Population
+ { countryID :: Int
+ , countryNameP :: String
+ , pop2010 :: Int
+ , pop2015 :: Int
+ , pop2021 :: Int
+ , capital :: String
+ }
+
+instance Show Population where
+   show population = mconcat [ show $ countryID population
+                             , ".) "
+                             , countryNameP population
+                             , ", Capital: "
+                             , capital population
+                             , ", 2010: "
+                             , show $ pop2010 population
+                             , ", 2015: "
+                             , show $ pop2015 population
+                             , ", 2021: "
+                             , show $ pop2021 population
+                             , "\n"]
+
+data GDP = GDP
+ { countryNameG :: String
+ , gdp2010 :: Int
+ , gdp2015 :: Int
+ , gdp2021 :: Int
+ }
+
+instance Show GDP where
+   show gdp = mconcat [ countryNameG gdp
+                      , ", 2010: "
+                      , show $ gdp2010 gdp
+                      , ", 2015: "
+                      , show $ gdp2015 gdp
+                      , ", 2021: "
+                      , show $ gdp2021 gdp
+                      , "\n"]
