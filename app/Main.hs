f41dc0d8f5d0c02732aecbe1bed6801bab47bace
soham7372 <soham.khawte2024@gmail.com>
2023-12-14 19:56:59 +0000

    fix: loop program

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,31 @@ import Fetch (downloadURLS, getURLConstructor)
 import Database
 import Parse (getGDP, getPOP)
 
--- To Ensure the prompt is displayed before reading input
--- prompt :: String -> IO String
--- prompt text = do
---     putStr text
---     hFlush stdout  
---     getLine
+nameToBeDetermined = do
+  putStrLn("\n\n1 - Fetch population of a country")
+  putStrLn("2 - Fetch GDP of a country")
+  putStrLn("3 - Fetch both Population and GDP of a country")
+  putStrLn("4 - Display Population data of all the countries")
+  putStrLn("5 - Display GDP data of all countries")
+
+  option <- prompt "\nSelect the option you want: "
+  case option of
+      "1" -> do
+        countryName <- prompt "\nEnter the country name: "
+        year <- prompt "\nEnter the year (2010, 2015, or 2021): "
+        fetchPopulation countryName year
+      "2" -> do
+        countryName <- prompt "\nEnter the country name: "
+        year <- prompt "\nEnter the year (2010, 2015, or 2021): "
+        fetchGDP countryName year
+      "3" -> do
+        countryName <- prompt "\nEnter the country name: "
+        year <- prompt "\nEnter the year (2010, 2015, or 2021): "
+        fetchPopulationAndGDP countryName year
+      -- "4" -> displayAllPopulationData
+      -- "5" -> displayAllGDPData
+      _ -> nameToBeDetermined
+      
 
 -- | The Main function
 main :: IO ()
@@ -29,6 +48,7 @@ main = do
   savePOPData popData
   saveGDPData gdpData 
   putStrLn "Data Added Succesfully!"
+  nameToBeDetermined
 
   putStrLn("\n\n1 - Fetch population of a country")
   putStrLn("2 - Fetch GDP of a country")
@@ -53,10 +73,3 @@ main = do
       -- "4" -> displayAllPopulationData
       -- "5" -> displayAllGDPData
       _ -> putStrLn "Invalid option selected. Please try again."
-
-  -- Testing Data
-  -- fetchGDP "United Kingdom" "2015"
-  -- fetchPopulation "Austria" "2015"
-
-  -- | Fetching and displaying GDP and Population data for a specific country and year.
-  -- fetchData
