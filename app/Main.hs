c939105a9d895fadf86779c2ea53e69e891c11ae
Rayaan Hussain <md.rayaan.h@gmail.com>
2023-12-14 12:37:38 +0000

    fix: move urls out

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Fetch 
+import Fetch (downloadURLS, getURLConstructor) 
 import Parse
 import Types
 import Database
@@ -9,21 +9,15 @@ import Database
 main :: IO ()
 main = do
   -- Fetching
-  let dataUN = URLS {gdp_url = "https://data.un.org/_Docs/SYB/CSV/SYB66_230_202310_GDP%20and%20GDP%20Per%20Capita.csv", pop_url = "https://data.un.org/_Docs/SYB/CSV/SYB66_1_202310_Population,%20Surface%20Area%20and%20Density.csv", isZip = False}
-  
-  let dataWorldBank = URLS {gdp_url = "https://api.worldbank.org/v2/en/indicator/NY.GDP.MKTP.CD?downloadformat=csv", pop_url = "https://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=csv", isZip = True}
+  let dataUn = getURLConstructor "UN"
 
-  -- _ = if not $ checkURLS dataUN then return () else True
-
-  -- let files = CSVFiles { gdpf "gdp", popf "pop"}
-
-  downloadURLS dataUN
+  downloadURLS dataUn
 
   -- Parsing
 
   parseCSV "gdp.csv"
 
-  -- Database
+  -- Database Operations
 
   createTables
   mapM_ addPopulation popData
